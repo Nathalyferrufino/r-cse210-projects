dc3f64d2b417d4a037ee1583bb568633aa809318
NathalyFerrufino <Nathalyferrufino@gmail.com>
2025-03-16 22:51:08 -0600

    Finished excersice 1!

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,12 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name: ");
+        Console.Write("What is your first name? ");
         string name = Console.ReadLine();
-        Console.Write("What is yoyrz last your first name: ");
+
+        Console.Write("What is your last name? ");
         string lastname = Console.ReadLine();
+        
         Console.WriteLine(" ");
  
-        Console.WriteLine ($"Your name is {name}, {lastname} {name}.");
+        Console.WriteLine ($"Your name is {lastname}, {name} {lastname}.");
     }
 }
+// Finished!
