01ccc7bd733c7338b55c14f7b972fa274a78b7e5
NathalyFerrufino <Nathalyferrufino@gmail.com>
2025-03-16 22:43:25 -0600

    Finished excersice 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        // Console.Write ("What is the magic number? ");
+        // int magicNumber = int.Parse (Console.ReadLine());
+
+        Random randomNumber = new Random ();
+        int number = randomNumber.Next (1, 100);
+
+        int guess = -1;
+        int attempts = 0;
+
+        while (guess != number)
+        {
+
+            Console.Write ("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (number > guess)
+            {
+                Console.WriteLine ("Higher");
+                attempts++;
+            }
+
+            else if (number < guess)
+            {
+                Console.WriteLine ("Lower");
+                attempts++;
+            }
+
+            else
+            {
+                Console.WriteLine ("You guessed it!");
+            }
+
+        }
+
+        Console.WriteLine ($"You did {attempts}!");
     }
 }
